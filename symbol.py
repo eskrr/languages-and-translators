e638570e454638d5c75aa531a62fd04bc75fddb8
eskrr <seesga97@gmail.com>
2019-11-20 20:00:19 -0600

    Delete unused library

--- symbol.py
+++
@@ -1,6 +0,0 @@
-class symbol:
-    def __init__(self, variable_id, variable_type, variable_address, function_index):
-        self.id = variable_id
-        self.type = variable_type
-        self.value = variable_address
-        self.index = function_index
